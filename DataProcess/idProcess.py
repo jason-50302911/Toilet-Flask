f54477f6a0f640674f2336ac61c90858cb292765
jason-50302911 <s50302911@gmail.com>
2024-06-15 05:18:33 +0800

    second commit

---
+++ DataProcess/idProcess.py
@@ -0,0 +1,46 @@
+import json
+
+from positionProcess import write_data
+from tqdm.auto import tqdm
+
+def create_empty_dict(input_des):
+    start_number = ord('A')
+    for index in range(0, 26):
+        alphabet = start_number + index
+        input_des[chr(alphabet)] = {}
+        
+def check_city_id(check_dict: dict, city_id: str) -> str: 
+    exist = False
+    for id in check_dict.keys():
+        if id == city_id:
+            exist = True
+            break
+    if exist is False:
+        check_dict[city_id] = []         
+        
+def sort_id(input_data: list) -> dict:
+    init_dict = {}
+    create_empty_dict(init_dict)
+    
+    for sample in tqdm(input_data):
+        key = sample["number"][0]
+        
+        check_dict = init_dict[key]
+        city_id = sample["number"][1:3]
+        
+        check_city_id(check_dict=check_dict, city_id=city_id)
+        
+        init_dict[key][city_id].append(sample)
+        
+    return init_dict
+
+if __name__ == "__main__":
+    try:
+        with open("data/toilet_data.json", mode="r", encoding="utf-8-sig") as file:
+            toilet_data_json = json.load(file)
+        id_dict = sort_id(input_data=toilet_data_json)
+        for country, city in id_dict.items():
+            print(country, len(city))
+        write_data(input_data=id_dict, file_name="idDict")
+    except Exception as error:
+        print(f"Error message: {error}")
---
+++ DataProcess/positionProcess.py
@@ -0,0 +1,121 @@
+import json
+import os
+
+from typing import Union
+from tqdm.auto import tqdm
+from pathlib import Path
+
+def create_empty_dict(input_dict: dict) -> dict:
+    for lng_id in range(117, 124, 1):
+        input_dict[str(lng_id)] = {}
+        
+    for lng in input_dict.values():
+        for lat_id in range(20, 28):
+            lng[str(lat_id)] = {}
+    
+    for lng_id in input_dict.values():
+        for lat_id in lng_id.values():
+            for float_id in range(0, 10000):
+                lat_id[str(float_id)] = []
+    
+            
+def id_encoder(number_lat: str, number_lng: str) -> int:
+    
+    lat_identity = number_lat[0:6]
+    lng_identity = number_lng[0:7]
+    
+    lat_id = int(lat_identity[0:2])
+    lng_id = int(lng_identity[0:3])
+    
+    float_id = int(lat_identity[3:5] + lng_identity[4:6])
+
+    return lat_id, lng_id, float_id
+
+
+def create_id_data(data: list, sample: dict, index: int, number_check: int) -> int:
+    check_flag = False
+    
+    cut_lat = sample["latitude"][:7]
+    cut_lng = sample["longitude"][:8]
+    
+    store_block = {
+        "uuid": index,
+        "id": [sample["number"]],
+        "lat": cut_lat,
+        "lng": cut_lng
+    }
+    
+    if len(data) == 0:
+        data.append(store_block)
+        number_check += 1
+    else:
+        for exist in data:
+            if exist["lat"] == cut_lat and exist["lng"] == cut_lng:
+                exist["id"].append(sample["number"])
+                number_check += 1
+                check_flag = True
+        if check_flag is False:
+            data.append(store_block)
+            number_check += 1
+    
+    return number_check
+    
+
+
+def write_id_list(input_data: list) -> list:
+    id_dict = {}
+    create_empty_dict(input_dict=id_dict)
+    
+    number_check = 0
+
+    for index, sample in tqdm(enumerate(input_data)):
+        
+        lat_id, lng_id, float_id = id_encoder(number_lat=sample["latitude"], 
+                                              number_lng=sample["longitude"])
+
+        
+        number_check = create_id_data(data=id_dict[str(lng_id)][str(lat_id)][str(float_id)], sample=sample, index=index, number_check=number_check)
+        
+    print(f"Check Number: {number_check}")
+        
+    return id_dict
+
+def write_data(input_data: Union[dict, list],
+               file_name: str):
+    
+    write_mode = None
+    
+    data_path = Path("data/")
+    file_path = (data_path / file_name).with_suffix(".json")
+    
+    if os.path.exists(file_path): 
+        file_size = file_path.stat().st_size
+        if file_size > 10: 
+            print(f"{file_name} file has been writen, skip writing...")
+        else: 
+            print(f"The {file_name} file is empty, starting writing file...")
+            write_mode = True
+    else:
+        write_mode = True
+         
+    if write_mode:
+        try:
+            with open(file_path, mode="w", encoding="utf-8") as file:
+                json.dump(input_data, file,  ensure_ascii=False, indent=4)
+                print(f"\nCreating data successfully")
+        except Exception as error:
+            print(f"\nWriting file error: {error}")
+
+
+
+
+if __name__ == "__main__":
+    try: 
+        with open("data/toilet_data.json", mode="r", encoding="utf-8-sig") as file:
+            toilet_data_json = json.load(file)
+            
+        id_dict = write_id_list(toilet_data_json)
+        write_data(input_data=id_dict, file_name="posid")
+    except Exception as error:
+        print(f"Error message: {error}")
+
